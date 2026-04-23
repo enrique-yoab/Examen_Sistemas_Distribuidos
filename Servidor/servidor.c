b6d04bd4a61940117b284195ea9fe64f3aa79612
enrique-yoab <yobi_jovi@hotmail.com>
2026-04-23 11:41:12 -0600

    Se regresa el antiguo codigo de pruebas

--- Servidor/servidor.c
+++ Servidor/servidor.c
@@ -2,75 +2,98 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include "./extensiones/entidades.h"
 #include "./extensiones/consulta.h"
 #include "./extensiones/insercion.h"
 
-#define PUERTO 3000
-#define TAM_MAX 500 
-// Variables globales
+// Variables globales (se mantienen igual)
 ARCHIVERO directorio;
 char *carpeta = "./Datos/";
 char *extension = ".csv";
 char *archivos[] = {"Estudiante", "Direccion", "Carrera", "Historial", "Inscripcion", "Seccion", "Profesor", "Departamento", "Niveles", "Horario", "Grado", "Curso", "Años", "Semestre"};
 int tamano = sizeof(archivos) / sizeof(archivos[0]);
 
-
 // Prototipo corregido (debe retornar void* y recibir void*)
 void* funcion_h1(void *arg);
 
 int main(void){
     // Variables para el hilo
-    pthread_t hilo1; HILO_DIR datos; 
+    pthread_t hilo1; HILO_DIR datos;
     // Llenamos la estructura con los datos
     datos.dir = &directorio;
     datos.carpeta = carpeta;
     datos.archivos = archivos;
     datos.extension = extension;
     datos.tamano = tamano;
+
     // 1. Creamos el hilo pasándole la dirección de 'datos'
     pthread_create(&hilo1, NULL, funcion_h1, (void *)&datos);
     pthread_join(hilo1, NULL);
 
-    // Levantamos el servicio y obtenemos el socket principal
-    int server_fd = levantar_servicio(PUERTO);
-    
-    int cliente_fd;
-    struct sockaddr_in dest;
-    socklen_t tam_dest = sizeof(dest);
-    char buffer_recepcion[TAM_MAX];
+    CONSULTA cliente;
+    cliente.numero_tabla = 5;             // 0 = Estudiante
+    cliente.error = NULL;
+    cliente.llave = "full";                // El snum que quieres buscar
+    cliente.parametros = "110000000";     // Máscara: queremos columnas 0, 1 y 2 (Snum, DNI, Nombre)
+    cliente.cantidad_resultados = 0;
+    cliente.resultado = NULL;              // Se llenará en la función
 
-    printf("Esperando conexiones entrantes de clientes...\n");
+    printf("\n--- Iniciando consulta de prueba ---\n");
+    solicitud_consulta(&cliente, &directorio);
 
-    // 5. Aceptar la conexión (Se bloquea aquí hasta que el cliente se conecte)
-    if ((cliente_fd = accept(server_fd, (struct sockaddr *)&dest, &tam_dest)) < 0) {
-        perror("Fallo en accept");
-        exit(EXIT_FAILURE);
+    if (cliente.error != NULL)
+    {
+        printf("%s", cliente.error);
+    }else if (cliente.cantidad_resultados > 0) {
+        
+        printf("Se encontró %d registros:\n", cliente.cantidad_resultados);
+
+        for (int i = 0; i < cliente.cantidad_resultados; i++) {
+            printf("Resultado [%d]: %s\n", i, cliente.resultado[i]);
+            free(cliente.resultado[i]); // Importante liberar la memoria de cada string
+        }
+        free(cliente.resultado); // Liberar el arreglo de punteros
     }
 
-    printf("\n[+] ¡Cliente conectado exitosamente!\n");
+    // Se inserta un año nuevo 
+    YEAR insert1;
+    insert1.ID_año = "1555";
+    insert1.Año = "2056";
 
-    // --- PRUEBA DE COMUNICACIÓN INICIAL ---
-    bzero(buffer_recepcion, TAM_MAX);
-    
-    
-    char *respuesta = "Bienvenido al Gestor de Base de Datos Distribuida\n";
-    send(cliente_fd, respuesta, strlen(respuesta), 0);
-    
-    // Con TCP usamos 'recv' y 'send' en lugar de 'recvfrom' y 'sendto'
-    recv(cliente_fd, buffer_recepcion, TAM_MAX, 0);
-    printf("El cliente dice: %s\n", buffer_recepcion);
-    // ... (Aquí irá tu lógica para desarmar el string, llenar las estructuras CONSULTA/INSERCION y procesarlas) ...
+    // Se simula la insercion del cliente
+    INSERCION cliente2;
+    cliente2.numero_tabla = 12;
+    cliente2.error = NULL;
+    cliente2.estructura = (void*)&insert1;
 
-    // Al terminar, cierras el socket del cliente
-    close(cliente_fd);
-    
-    // Y si el servidor se apagara, cierras el principal
-    close(server_fd);
+    printf("\n--- Iniciando insercion de prueba ---\n");
+    solicitud_insercion(&cliente2, &directorio);
+    printf("%s\n", cliente2.error);
 
+
+    cliente.numero_tabla = 12;             // 0 = Estudiante
+    cliente.error = NULL;
+    cliente.llave = "full";                // El snum que quieres buscar
+    cliente.parametros = "01";     // Máscara: queremos columnas 0, 1 y 2 (Snum, DNI, Nombre)
+    cliente.cantidad_resultados = 0;
+    cliente.resultado = NULL;  
+    // Consultamos nuevamente
+    printf("\n--- Iniciando consulta de prueba ---\n");
+    solicitud_consulta(&cliente, &directorio);
+
+    if (cliente.error != NULL)
+    {
+        printf("%s", cliente.error);
+    }else if (cliente.cantidad_resultados > 0) {
+        
+        printf("Se encontró %d registros:\n", cliente.cantidad_resultados - 1);
+
+        for (int i = 0; i < cliente.cantidad_resultados; i++) {
+            printf("Resultado [%d]: %s\n", i, cliente.resultado[i]);
+            free(cliente.resultado[i]); // Importante liberar la memoria de cada string
+        }
+        free(cliente.resultado); // Liberar el arreglo de punteros
+    }
     return 0;
 }
 
